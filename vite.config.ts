14d8f338b09926abf84aae87a03cecc682af6aa7
Diego Sullcaray Ramos <u20211c348@upc.edu.pe>
2025-10-02 00:13:04 -0500

    dd

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     proxy: {
       '/api': {
         target: 'https://edugen-backend.brianuceda.xyz',
@@ -12,6 +13,15 @@ export default defineConfig({
         secure: true,
       }
     }
+  },
+  preview: {
+    host: true,
+    allowedHosts: [
+      'edugen.brianuceda.xyz',
+      'www.edugen.brianuceda.xyz',
+      'localhost',
+      '127.0.0.1'
+    ]
   },
   build: {
     rollupOptions: {
